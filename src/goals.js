5d5cc3a6d54726db74ee64ddf4573a554dd0ace6
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-07-14 14:29:01 +0200

    GOals list service

--- src/goals.js
+++ src/goals.js
@@ -1,4 +1,11 @@
 const {Router} =require('express')
+const {goals:goalsDB} = require('./db')
 const router = new Router()
+
+router.get('/',(req,res,next)=>{
+  goalsDB.allDocs({include_docs:true}).then(response=>response.rows.map(row=>row.doc))
+  .then(goals=>res.send(goals))
+  .catch((err)=>res.status(500).send(err))
+})
 
 module.exports = router
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(require('./goals'))
-app.use(express.static(path.join(__dirname,'www')))
-app.use(express.static(path.join(__dirname,'generated')))
+app.use('/goals', require('./goals'))
+app.use(express.static(path.join(__dirname, 'www')))
+app.use(express.static(path.join(__dirname, 'generated')))
 
 app.listen(3000, () => console.log('Command Center listening on port 3000!'))
