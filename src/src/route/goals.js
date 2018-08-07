a8d7ca568cf4029cbd55e0d13a92418780fe4130
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-08-07 17:34:42 +0200

    separated list to child component

--- src/src/route/goals.js
+++ src/src/route/goals.js
@@ -8,35 +8,42 @@ export default [{
   name: 'goals',
   path: 'goals',
   component: {
-    template: `<div class="row">
-      <div class="col-12">
-        <table class="table">
-          <tr>
-            <th>AOC</th>
-            <th>Project</th>
-            <th>Goals</th>
-          </tr>
-          <tr v-for="goal in goals">
-            <td>{{goal.aoc}} </td>
-            <td>{{goal.project.name}} </td>
-            <td>{{goal.target}} </td>
-          </tr>
-        </table>
+    template: `<router-view class="mt-1"></router-view>`
+  },
+  children: [{
+    name: 'goals.list',
+    path: '',
+    component: {
+      template: `<div class="row">
+        <div class="col-12">
+          <table class="table">
+            <tr>
+              <th>AOC</th>
+              <th>Project</th>
+              <th>Goals</th>
+            </tr>
+            <tr v-for="goal in goals">
+              <td>{{goal.aoc}} </td>
+              <td>{{goal.project.name}} </td>
+              <td>{{goal.target}} </td>
+            </tr>
+          </table>
+        </div>
       </div>
-    </div>
-    `,
-    beforeRouteEnter(to, from, next) {
-      list().then(goals => next(vm => {
-        vm.goals = goals
-      }))
-    },
-    beforeRouteUpdate(to, from, next) {
-      list().then(goals => {
-        this.goals = goals
+      `,
+      beforeRouteEnter(to, from, next) {
+        list().then(goals => next(vm => {
+          vm.goals = goals
+        }))
+      },
+      beforeRouteUpdate(to, from, next) {
+        list().then(goals => {
+          this.goals = goals
+        })
+      },
+      data: () => ({
+        goals: []
       })
     },
-    data: () => ({
-      goals: []
-    })
-  }
+  }]
 }]
