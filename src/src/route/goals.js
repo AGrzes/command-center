511d52008d02c074ed6d80951b5d724c755eaeb7
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-08-15 21:27:55 +0200

    Component to pass goal

--- src/src/route/goals.js
+++ src/src/route/goals.js
@@ -82,6 +82,50 @@ Vue.component('goal-tags', {
   template: '<span><span class="badge badge-primary badge-pill mr-1" v-for="tag in tags">{{tag}}</span></span>'
 })
 
+Vue.component('goal-pass', {
+  props: ['goal'],
+  template: `
+  <span>
+    <portal to="modal">
+      <div class="modal" tabindex="-1" role="dialog" ref="modal">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Pass goal</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <div class="form-group">
+                <input type="text" class="form-control"  placeholder="Comment" v-model="comment">
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary" @click="pass()">Pass</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </portal>
+    <a class="btn btn-primary btn-sm" @click="showModal()">Pass</a>
+  </span>`,
+  methods: {
+    showModal() {
+      $(this.$refs.modal).modal()
+    },
+    pass(){
+      $(this.$refs.modal).modal('hide')
+    }
+  },
+  data() {
+    return {
+      comment: null
+    }
+  }
+})
+
 Vue.component('goal-item', {
   props: ['goal'],
   data: () => ({
@@ -95,7 +139,8 @@ Vue.component('goal-item', {
       <goal-measurement :measurement="goal.measurement" class="ml-auto w-25 mr-4"></goal-measurement>
       <goal-result :result="goal.result" class="mr-1"></goal-result>
       <a @click="expanded=!expanded" class="btn btn-primary btn-sm mr-1">{{expanded?'V':'>'}}</a>
-      <router-link tag="a" class="btn btn-primary btn-sm" :to="{name:'goals.details',params: { id: goal._id }}">+</router-link>
+      <goal-pass goal="goal" class="mr-1"></goal-pass>
+      <router-link tag="a" class="btn btn-primary btn-sm mr-1" :to="{name:'goals.details',params: { id: goal._id }}">+</router-link>
     </div>
     <div class="row" v-if="expanded">
       <goal-description :description="goal.description" class="col-12 col-md-6"></goal-description>
