64613f7cd6f6d78b426e7ba8571ee3df40296f14
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-08-18 21:16:05 +0200

    Global archive

--- src/src/route/goals.js
+++ src/src/route/goals.js
@@ -300,6 +300,61 @@ Vue.component('goal-details', {
   `
 })
 
+Vue.component('goals-archive', {
+  props: ['goals'],
+  template: `
+  <span>
+    <portal to="modal">
+      <div class="modal" tabindex="-1" role="dialog" ref="modal">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Archive Goals</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <div class="form-group">
+                <input type="date" class="form-control"  placeholder="Date" v-model="date">
+              </div>  
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary" @click="doArchive()">Archive</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </portal>
+    <a class="btn btn-primary btn-sm" @click="showModal()">Archive</a>
+  </span>`,
+  methods: {
+    showModal() {
+      $(this.$refs.modal).modal()
+    },
+    doArchive() {
+      this.goals.forEach((goal)=>{
+        if (!goal.archive && goal.measurement.done && moment(goal.measurement.done).isSameOrBefore(this.date,'day')){
+          goal.history.push({
+            event: 'archive',
+            date: moment().toISOString()
+          })
+          goal.measurement.done = moment().toISOString()
+          goal.archive = true
+          save(goal)
+        }
+      })
+      $(this.$refs.modal).modal('hide')
+    }
+  },
+  data() {
+    return {
+      date: moment().format('YYYY-MM-DD')
+    }
+  }
+})
+
 export default [{
   name: 'goals',
   path: 'goals',
@@ -310,8 +365,16 @@ export default [{
     name: 'goals.list',
     path: '',
     component: {
-      template: `<div class="row">
+      template: `
+      <div class="row">
         <div class="col-12">
+          <div class="card">
+            <div class="card-body">
+              <goals-archive :goals="goals"></goals-archive>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 mt-1">
           <ul class="list-group">
             <li class="list-group-item" v-for="goal in goals">
               <goal-item :goal="goal"></goal-item>
