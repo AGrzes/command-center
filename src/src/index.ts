6e3527fd9dda2e899ed4dcfc0776416e8257c955
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-08-25 22:49:45 +0200

    Corrected static paths

--- src/src/index.ts
+++ src/src/index.ts
@@ -1,6 +1,5 @@
 import * as history from 'connect-history-api-fallback'
 import * as express from 'express'
-import * as  path from 'path'
 import {router as goals} from './goals'
 import {router as reminders} from './reminders'
 const app = express()
@@ -8,7 +7,7 @@ const app = express()
 app.use('/api/goals', goals)
 app.use('/api/reminders', reminders)
 app.use(history())
-app.use(express.static(path.join(__dirname, 'www')))
-app.use(express.static(path.join(__dirname, 'web/generated')))
+app.use(express.static('www'))
+app.use(express.static('web/generated'))
 // tslint:disable-next-line:no-console
 app.listen(3000, () => console.log('Command Center listening on port 3000!'))
