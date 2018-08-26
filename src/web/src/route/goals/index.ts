8799f5d5a68557205a9741039878f60e1982dacf
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-08-26 21:08:05 +0200

    Removed innecesary imports

--- src/web/src/route/goals/index.ts
+++ src/web/src/route/goals/index.ts
@@ -1,8 +1,3 @@
-import * as $ from 'jquery'
-import * as _ from 'lodash'
-import * as moment from 'moment'
-import Vue from 'vue'
-import {create, item, list, save} from './api'
 import './components'
 import routes from './routes'
 
