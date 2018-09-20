cba2852036358c2171cd2ddb6609d5a7ca4888f2
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-09-20 21:54:12 +0200

    defined and resolved progress

--- src/src/db.ts
+++ src/src/db.ts
@@ -1,7 +1,8 @@
 import * as PouchDB from 'pouchdb-http'
+import * as PouchDBMapReduce from 'pouchdb-mapreduce'
 import { ProgressItem } from './model/progress'
 import { Reminder } from './model/reminders'
-
+PouchDB.plugin(PouchDBMapReduce)
 export const goals = new PouchDB('http://couchdb:5984/goals')
 
 export const reminders = new PouchDB<Reminder>('http://couchdb:5984/reminders')
--- src/src/progress.ts
+++ src/src/progress.ts
@@ -8,3 +8,15 @@ router.get('/', (req, res, next) => {
     .then((items) => res.send(items))
     .catch((err) => res.status(500).send(err))
 })
+router.get('/resolved', (req, res, next) => {
+  progressDB.query('queries/resolved', {include_docs: true, limit: 100, descending: true})
+  .then((response) => response.rows.map(({doc}) => (doc)))
+    .then((items) => res.send(items))
+    .catch((err) => res.status(500).send(err))
+})
+router.get('/defined', (req, res, next) => {
+  progressDB.query('queries/defined', {include_docs: true, limit: 100, descending: true})
+  .then((response) => response.rows.map(({doc}) => (doc)))
+    .then((items) => res.send(items))
+    .catch((err) => res.status(500).send(err))
+})
