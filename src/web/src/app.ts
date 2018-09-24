6ee4d20130d7f06c4eb668deb902f5f5eaf7a63d
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-09-24 21:22:27 +0200

    Apex chart import

--- src/web/src/app.ts
+++ src/web/src/app.ts
@@ -1,7 +1,9 @@
 import * as PortalVue from 'portal-vue'
 import Vue from 'vue'
+import VueApexCharts from 'vue-apexcharts'
 import router from './router'
 
+Vue.use(VueApexCharts)
 Vue.use(PortalVue)
 
 const app = new Vue({
