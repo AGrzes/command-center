5da566780dea2bc796d92bb47a14c07e2f35561f
Grzegorz Abramczyk <abramczykg@gmail.com>
2018-09-29 20:58:49 +0200

    Groupped results

--- src/src/progress.ts
+++ src/src/progress.ts
@@ -8,14 +8,46 @@ router.get('/', (req, res, next) => {
     .then((items) => res.send(items))
     .catch((err) => res.status(500).send(err))
 })
+router.get('/:query', (req, res) => {
+  const limit = req.query.limit || 100
+  switch (req.query.group) {
+    case 'day':
+    progressDB.query(`queries/${req.params.query}`, {
+      limit, descending: true, group: true, group_level: 3
+    })
+      .then((items) => res.send(items))
+      .catch((err) => res.status(500).send(err))
+    break
+    case 'month':
+    progressDB.query(`queries/${req.params.query}`, {
+      limit, descending: true, group: true, group_level: 2
+    })
+      .then((items) => res.send(items))
+      .catch((err) => res.status(500).send(err))
+    break
+    case 'year':
+    progressDB.query(`queries/${req.params.query}`, {
+      limit, descending: true, group: true, group_level: 1
+    })
+      .then((items) => res.send(items))
+      .catch((err) => res.status(500).send(err))
+    break
+    default:
+    progressDB.query(`queries/${req.params.query}`, {include_docs: true, limit, descending: true})
+    .then((response) => response.rows.map(({doc}) => (doc)))
+      .then((items) => res.send(items))
+      .catch((err) => res.status(500).send(err))
+  }
+
+})
 router.get('/resolved', (req, res, next) => {
-  progressDB.query('queries/resolved', {include_docs: true, limit: 100, descending: true})
+  progressDB.query('queries/resolved', {include_docs: true, limit: 100, descending: true, reduce: false})
   .then((response) => response.rows.map(({doc}) => (doc)))
     .then((items) => res.send(items))
     .catch((err) => res.status(500).send(err))
 })
 router.get('/defined', (req, res, next) => {
-  progressDB.query('queries/defined', {include_docs: true, limit: 100, descending: true})
+  progressDB.query('queries/defined', {include_docs: true, limit: 100, descending: true, reduce: false})
   .then((response) => response.rows.map(({doc}) => (doc)))
     .then((items) => res.send(items))
     .catch((err) => res.status(500).send(err))
